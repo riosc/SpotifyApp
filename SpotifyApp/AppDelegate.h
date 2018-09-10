070534ff6ced4a8c4a00fcd4766734a0966b336f
Carlos David Rios Vertel <rioscarlosd@gmail.com>
2018-09-10 17:52:22 -0300

    empty projects

---
+++ SpotifyApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SpotifyApp
+//
+//  Created by Carlos David Rios Vertel on 9/10/18.
+//  Copyright © 2018 Carlos David Rios Vertel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SpotifyApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SpotifyApp
+//
+//  Created by Carlos David Rios Vertel on 9/10/18.
+//  Copyright © 2018 Carlos David Rios Vertel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
